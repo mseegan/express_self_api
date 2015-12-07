2799b715512b66787ff8e00a6aa99fef3e19c5dc
mseegan <mseegan@gmail.com>
2015-12-07 13:29:37 -0800

    added everything

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,124 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-// your code
+  
+  var baseUrl = '/api/vidyas';
 
+  
+  var allvidyas = [];
+
+  // element to display list of vidyas
+  var $vidyasList = $('#vidyas-list');
+
+  // form to create new vidya
+  var $createvidya = $('#create-vidya');
+
+
+ 
+
+  // helper function to render all vidyas to view
+  // note: we empty and re-render the collection each time our vidya data changes
+  var render = function() {
+    // empty existing vidyas from view
+    $vidyasList.empty();
+
+    // pass `allvidyas` into the template function
+    var vidyasHtml = template({ vidyas: allvidyas });
+
+    // append html to the view
+    $vidyasList.append(vidyasHtml);
+  };
+
+  // GET all vidyas on page load
+  $.get(baseUrl, function (data) {
+    console.log(data);
+
+    // set `allvidyas` to vidya data from API
+    allvidyas = data.vidyas;
+
+    // render all vidyas to view
+    render();
+  });
+
+  // listen for submit even on form
+  $createvidya.on('submit', function (event) {
+    event.preventDefault();
+
+    // serialze form data
+    var newvidya = $(this).serialize();
+
+    // POST request to create new vidya
+    $.post(baseUrl, newvidya, function (data) {
+      console.log(data);
+
+      // add new vidya to `allvidyas`
+      allvidyas.push(data);
+
+      // render all vidyas to view
+      render();
+    });
+
+    // reset the form
+    $createvidya[0].reset();
+    $createvidya.find('input').first().focus();
+  });
+
+  // add event-handlers to vidyas for updating/deleting
+  $vidyasList
+
+    // for update: submit event on `.update-vidya` form
+    .on('submit', '.update-vidya', function (event) {
+      event.preventDefault();
+      
+      // find the vidya's id (stored in HTML as `data-id`)
+      var vidyaId = $(this).closest('.vidya').attr('data-id');
+
+      // find the vidya to update by its id
+      var vidyaToUpdate = allvidyas.filter(function (vidya) {
+        return vidya._id == vidyaId;
+      })[0];
+
+      // serialze form data
+      var updatedvidya = $(this).serialize();
+
+      // PUT request to update vidya
+      $.ajax({
+        type: 'PUT',
+        url: baseUrl + '/' + vidyaId,
+        data: updatedvidya,
+        success: function(data) {
+          // replace vidya to update with newly updated version (data)
+          allvidyas.splice(allvidyas.indexOf(vidyaToUpdate), 1, data);
+
+          // render all vidyas to view
+          render();
+        }
+      });
+    })
+    
+    // for delete: click event on `.delete-vidya` button
+    .on('click', '.delete-vidya', function (event) {
+      event.preventDefault();
+
+      // find the vidya's id (stored in HTML as `data-id`)
+      var vidyaId = $(this).closest('.vidya').attr('data-id');
+
+      // find the vidya to delete by its id
+      var vidyaToDelete = allvidyas.filter(function (vidya) {
+        return vidya._id == vidyaId;
+      })[0];
+
+      // DELETE request to delete vidya
+      $.ajax({
+        type: 'DELETE',
+        url: baseUrl + '/' + vidyaId,
+        success: function(data) {
+          // remove deleted vidya from all vidyas
+          allvidyas.splice(allvidyas.indexOf(vidyaToDelete), 1);
+
+          // render all vidyas to view
+          render();
+        }
+      });
+    });
 });
--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 // require express and other modules
 var express = require('express'),
     app = express();
+    // require express and other modules
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser');
+
+// configure bodyParser (for receiving form data)
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
@@ -10,10 +17,29 @@ app.use(express.static(__dirname + '/public'));
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+
+  app.use(express.static(__dirname + '/public/images'));
+	console.log('use localhost:3000/profile_image.png');
+
+	app.get(function (req, res){
+		res.writehead(200, {'content-type': 'text/plain'});
+	});
 /************
  * DATABASE *
  ************/
+var profile = [
+{name: "Matt Seegan", github_link: "https://github.com/mseegan", 
+github_profile_image: "https:/\/avatars1.githubusercontent.com/u/15822069?v=3&s=460", 
+current_city: "San Francisco", 
+family_members: [{name: "Tom", relationship: "Father"}, 
+{name: "Kim", relationship: "Mother"}, 
+{name: "Brooke", relationship: "Sister"}]
+}];
 
+var vidya = [{_id: 1, title: "Dota 2", genre: "moba", singleplayer: false, multiplayer: true}, 
+{_id: 2, title: "Starcraft 2", genre: "rts", singleplayer: true, multiplayer: true}, 
+{_id: 3, title: "Hatoful Boyfriend", genre: "otome", singleplayer: true, multiplayer: false}
+];
 // your hardcoded data here
 
 /**********
@@ -24,9 +50,15 @@ app.use(express.static(__dirname + '/public'));
  * HTML Endpoints
  */
 
+
+
+	
+
+
 app.get('/', function homepage (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
+
 
 
 /*
@@ -42,6 +74,69 @@ app.get('/api', function api_index (req, res){
       {method: "GET", path: "/api", description: "Describes available endpoints"}
     ]
   });
+});
+
+app.get('/api/vidya/search', function search(req, res){
+	var data = {vidya:[]};
+	vidya.forEach(function (element, index) {
+		if(element.task == req.query.q){
+			console.log(req.query.q);
+			data.vidya.push(vidya[index]);
+		}
+	});
+	res.json(data);
+});
+
+
+app.get('/api/vidya', function index(req, res) {
+	res.json({vidya: vidya});
+});
+
+app.get('/api/profile', function index(req, res) {
+	res.json({profile: profile});
+});
+
+app.post('/api/vidya', function create(req, res) {
+	var data = req.body;
+	data._id = 0; 
+	vidya.forEach(function (element, index) {
+		data._id = element._id >= data._id ? element._id+1 : data._id;
+
+	});
+	vidya.push(data);
+	res.json(data);
+
+});
+
+app.get('/api/vidya/:id', function show(req, res) {
+	var data;
+	vidya.forEach(function (element, index) {
+		if(element._id == req.params.id){
+			data = vidya[index];
+		}
+	});
+	res.json(data);
+});
+
+app.put('/api/vidya/:id', function update(req, res) {
+	var data = req.body;
+	data._id = 0; 
+	vidya.forEach(function (element, index) {
+		data._id = element._id >= data._id ? element._id : data._id;
+
+	});
+	vidya.push(data);
+	res.json(data);
+});
+app.delete('/api/vidya/:id', function destroy(req, res) {
+	var data;
+	vidya.forEach(function (element, index) {
+		if(element._id == req.params.id){
+			data = vidya[index];
+			vidya.splice(index, 1);
+		}
+	});
+	res.json(data);
 });
 
 /**********
